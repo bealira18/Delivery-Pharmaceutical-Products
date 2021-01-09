0995309a4f4d1d27f213d5684b088716ab6f5de9
jcaboferreira <1190708@isep.ipp.pt>
2021-01-09 23:56:36 +0000

    <MEN06-15> MEN06-126 #close [Review]

--- src/test/java/lapr/project/controller/AddScooterControllerTest.java
+++ src/test/java/lapr/project/controller/AddScooterControllerTest.java
@@ -13,7 +13,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class AddScooterControllerTest {
-/*
+
     private static AddScooterController controller;
 
     public AddScooterControllerTest(){
@@ -23,17 +23,17 @@ class AddScooterControllerTest {
     public static void setUpClass() throws SQLException {
 
         Pharmacy pharmacy = new Pharmacy(1, "Coimbrões", "R. Domingos de Matos 680, 4405 Vila Nova de Gaia");
-        ScooterPark scooterPark = new ScooterPark(1,1,3,1,"R. Domingos de Matos 680, 4405 Vila Nova de Gaia");
         ScooterStatus scooterStatus = new ScooterStatus(1, "TestName");
+        ScooterPark scooterPark = new ScooterPark(1,1,1,1,"R. Domingos de Matos 680, 4405 Vila Nova de Gaia");
 
-        Scooter scooter1 = new Scooter(1, 1, 1,10, 100);
-        Scooter scooter2 = new Scooter(2, 1, 1,20,100);
-
+        Scooter scooter1 = new Scooter(1, 1, 20, 40, 50, 60, 10, 100, 1);
 
         ScooterDB scooterDB = mock(ScooterDB.class);
         ScooterParkDB scooterParkDB = mock(ScooterParkDB.class);
 
-
+        when(scooterDB.addScooter(scooter1)).thenReturn(Boolean.TRUE);
+        when(scooterParkDB.getNumberOfScootersInPharmacy(scooter1.getIdPharmacy())).thenReturn(0);
+        when(scooterParkDB.getLimitScooterPark(scooter1.getIdPharmacy())).thenReturn(1);
 
         controller = new AddScooterController();
         controller = new AddScooterController(scooterDB, scooterParkDB);
@@ -44,29 +44,34 @@ class AddScooterControllerTest {
      *
      * @throws java.lang.Exception
      */
-   /* @Test
+    @Test
     public void testAddScooter() throws Exception {
 
         System.out.println("addScooter");
 
         Pharmacy pharmacy = new Pharmacy(1, "Coimbrões", "R. Domingos de Matos 680, 4405 Vila Nova de Gaia");
-        ScooterPark scooterPark = new ScooterPark(1,1,3,1,"R. Domingos de Matos 680, 4405 Vila Nova de Gaia");
         ScooterStatus scooterStatus = new ScooterStatus(1, "TestName");
-        Scooter scooter3 = new Scooter(3, 1, 1,30, 100);
+        ScooterPark scooterPark = new ScooterPark(1,1,1,1,"R. Domingos de Matos 680, 4405 Vila Nova de Gaia");
 
-        boolean expResult = true;
-        boolean result = controller.addScooter(scooter3);
-        assertEquals(expResult, result);
+        Scooter scooter1 = new Scooter(1, 1, 20, 40, 50, 60, 10, 100, 1);
+
+        boolean result = controller.addScooter(scooter1);
+        assertEquals(true, result);
 
         ScooterDB scooterDB = mock(ScooterDB.class);
         ScooterParkDB scooterParkDB = mock(ScooterParkDB.class);
-        when(scooterDB.addScooter(new Scooter(2, 1, 1, 15, 100))).thenReturn(Boolean.FALSE);
+
+        when(scooterParkDB.getNumberOfScootersInPharmacy(scooter1.getIdPharmacy())).thenReturn(1);
+
         AddScooterController controller1 = new AddScooterController(scooterDB, scooterParkDB);
 
-        expResult = false;
-        result = controller1.addScooter(scooter3);
-        assertEquals(expResult, result);
+        result = controller1.addScooter(scooter1);
+        assertEquals(false, result);
 
+        when(scooterDB.addScooter(scooter1)).thenReturn(Boolean.FALSE);
+        when(scooterParkDB.getNumberOfScootersInPharmacy(scooter1.getIdPharmacy())).thenReturn(0);
+
+        result = controller1.addScooter(scooter1);
+        assertEquals(false, result);
     }
-*/
 }
