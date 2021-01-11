86fc788036e1f43e9b4656f076ffed7bf77e0e5b
António Dias <1182147@isep.ipp.pt>
2021-01-11 03:40:36 +0000

    <MEN06-173> Delivery Status Tests

--- src/main/java/lapr/project/model/Vehicle.java
+++ src/main/java/lapr/project/model/Vehicle.java
@@ -14,6 +14,7 @@ public class Vehicle {
     private double maxBattery;
 
     public Vehicle(int idVehicle, int idPharmacy, double weight, double aerodynamicCoeficient, double frontalArea, double motor, double currentBattery, double maxBattery) {
+
         this.idVehicle = idVehicle;
         this.idPharmacy = idPharmacy;
         this.weight = weight;
@@ -23,106 +24,116 @@ public class Vehicle {
         this.currentBattery = currentBattery;
         this.maxBattery = maxBattery;
     }
-
-    public Vehicle(int idVehicle) {
+    
+    public Vehicle(int idVehicle){
+        
         this.idVehicle = idVehicle;
     }
 
     public int getIdVehicle() {
+        
         return idVehicle;
     }
 
     public int getIdPharmacy() {
+        
         return idPharmacy;
     }
 
     public double getWeight() {
+        
         return weight;
     }
 
     public double getAerodynamicCoeficient() {
+        
         return aerodynamicCoeficient;
     }
 
     public double getFrontalArea() {
+        
         return frontalArea;
     }
 
     public double getMotor() {
+        
         return motor;
     }
 
     public double getCurrentBattery() {
+        
         return currentBattery;
     }
 
     public double getMaxBattery() {
+        
         return maxBattery;
     }
 
     public void setIdVehicle(int idVehicle) {
+        
         this.idVehicle = idVehicle;
     }
 
     public void setIdPharmacy(int idPharmacy) {
+        
         this.idPharmacy = idPharmacy;
     }
 
     public void setWeight(double weight) {
+        
         this.weight = weight;
     }
 
     public void setAerodynamicCoeficient(double aerodynamicCoeficient) {
+        
         this.aerodynamicCoeficient = aerodynamicCoeficient;
     }
 
     public void setFrontalArea(double frontalArea) {
+        
         this.frontalArea = frontalArea;
     }
 
     public void setMotor(double motor) {
+        
         this.motor = motor;
     }
 
     public void setCurrentBattery(double currentBattery) {
+        
         this.currentBattery = currentBattery;
     }
 
     public void setMaxBattery(double maxBattery) {
+        
         this.maxBattery = maxBattery;
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Vehicle)) return false;
-        Vehicle vehicle = (Vehicle) o;
-        return getIdVehicle() == vehicle.getIdVehicle() &&
-                getIdPharmacy() == vehicle.getIdPharmacy() &&
-                Double.compare(vehicle.getWeight(), getWeight()) == 0 &&
-                Double.compare(vehicle.getAerodynamicCoeficient(), getAerodynamicCoeficient()) == 0 &&
-                Double.compare(vehicle.getFrontalArea(), getFrontalArea()) == 0 &&
-                Double.compare(vehicle.getMotor(), getMotor()) == 0 &&
-                Double.compare(vehicle.getCurrentBattery(), getCurrentBattery()) == 0 &&
-                Double.compare(vehicle.getMaxBattery(), getMaxBattery()) == 0;
+    public String toString() {
+        return "Vehicle{" + "idVehicle=" + idVehicle + ", idPharmacy=" + idPharmacy + ", weight=" + weight + ", aerodynamicCoeficient=" + aerodynamicCoeficient + ", frontalArea=" + frontalArea + ", motor=" + motor + ", currentBattery=" + currentBattery + ", maxBattery=" + maxBattery + '}';
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getIdVehicle(), getIdPharmacy(), getWeight(), getAerodynamicCoeficient(), getFrontalArea(), getMotor(), getCurrentBattery(), getMaxBattery());
+        
+        int hash = 3;
+        hash = 13 * hash + this.idVehicle;
+        return hash;
     }
 
     @Override
-    public String toString() {
-        return "Vehicle{" +
-                "idVehicle=" + idVehicle +
-                ", idPharmacy=" + idPharmacy +
-                ", weight=" + weight +
-                ", aerodynamicCoeficient=" + aerodynamicCoeficient +
-                ", frontalArea=" + frontalArea +
-                ", motor=" + motor +
-                ", currentBattery=" + currentBattery +
-                ", maxBattery=" + maxBattery +
-                '}';
-    }
+    public boolean equals(Object obj) {
+        
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Vehicle other = (Vehicle) obj;
+        
+        return idVehicle == other.idVehicle;
+    }   
 }
