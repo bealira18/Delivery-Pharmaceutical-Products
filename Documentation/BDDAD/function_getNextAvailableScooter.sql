5ed9587a2eb240e78a512fe343284c50cd7f152d
André Silva [1171060] <1171060@isep.ipp.pt>
2021-01-11 23:58:04 +0000

    <MEN06-22> MEN06-146 #close [Implementation]

--- Documentation/BDDAD/function_getNextAvailableScooter.sql
+++ Documentation/BDDAD/function_getNextAvailableScooter.sql
@@ -3,7 +3,7 @@ RETURN SYS_REFCURSOR IS next_scooter SYS_REFCURSOR;
 
 BEGIN
     OPEN next_scooter FOR
-        SELECT d.id_vehicle, d.delivery_end
+        SELECT d.id_order, d.id_vehicle, d.email_courier, d.id_delivery_status, d.delivery_start, d.delivery_end, d.delivery_run
         FROM delivery d
         INNER JOIN scooter s
         ON d.id_vehicle = s.id_scooter
