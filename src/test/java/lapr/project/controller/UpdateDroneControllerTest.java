048bc10ddcd3adb59d176ae674f3c0e1fbb2fa05
bealira <1200607@isep.ipp.pt>
2021-01-13 00:02:55 +0000

    <MEN06-248> MEN06-273 #close [Review]

--- src/test/java/lapr/project/controller/UpdateDroneControllerTest.java
+++ src/test/java/lapr/project/controller/UpdateDroneControllerTest.java
@@ -1,6 +1,8 @@
 package lapr.project.controller;
 
+import lapr.project.data.DroneDB;
 import lapr.project.data.ScooterDB;
+import lapr.project.model.Drone;
 import lapr.project.model.Scooter;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -10,33 +12,33 @@ import java.sql.SQLException;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.mock;
 
-public class UpdateScooterControllerTest {
+public class UpdateDroneControllerTest {
 
-    private static UpdateScooterController sPC;
+    private static UpdateDroneController dPC;
 
-    public UpdateScooterControllerTest() {
+    public UpdateDroneControllerTest() {
     }
 
     @BeforeAll
     public static void setUpClass() {
 
-        ScooterDB pDB = mock(ScooterDB.class);
+        DroneDB dDB = mock(DroneDB.class);
 
-        sPC = new UpdateScooterController();
-        sPC = new UpdateScooterController(pDB);
+        dPC = new UpdateDroneController();
+        dPC = new UpdateDroneController(dDB);
     }
 
     @Test
-    void testUpdateScooter1() throws SQLException {
+    void testUpdateDrone1() throws SQLException {
         //int idVehicle, int idPharmacy, double weight, double aerodynamicCoeficient, double frontalArea, double motor, double currentBattery, double maxBattery)
-        Scooter s=new Scooter(0,0,2.0,2.5,1,2.0,35.0,40.0,0);
-        boolean actual=sPC.updateScooter(0,s);
+        Drone s=new Drone(0,0,2.0,2.5,1,2.0,35.0,40.0,0);
+        boolean actual=dPC.updateDrone(0,s);
         assertFalse(actual); //porque não existem scooters
     }
 
     @Test
-    void testUpdateScooter2() throws SQLException {
-        boolean actual=sPC.updateScooter(0,null);
+    void testUpdateDrone2() throws SQLException {
+        boolean actual=dPC.updateDrone(0,null);
         assertFalse(actual);
     }
 
