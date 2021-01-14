36d81e41ad3bfa7bd619da8a64fafd6ebcb15072
André Silva [1171060] <1171060@isep.ipp.pt>
2021-01-14 22:02:49 +0000

    MEN06-302 #comment adding scooter and drone park when pharmacy is created

--- Documentation/BDDAD/procedure_addPharmacy.sql
+++ Documentation/BDDAD/procedure_addPharmacy.sql
@@ -1,20 +1,29 @@
-CREATE OR REPLACE PROCEDURE ADDPHARMACY(a_name IN VARCHAR2, a_lat IN NUMERIC, a_lon IN NUMERIC,
-                                        a_alt IN NUMERIC, p_name IN VARCHAR2, p_limit IN INTEGER) IS
+CREATE OR REPLACE PROCEDURE ADDPHARMACY(
+    a_name IN VARCHAR2, 
+    a_lat IN NUMERIC, 
+    a_lon IN NUMERIC,
+    a_alt IN NUMERIC, 
+    p_name IN VARCHAR2, 
+    p_limit_scooter_park IN INTEGER,
+    p_limit_drone_park IN INTEGER) IS
 
     v_pharmacy_id       NUMBER;
 
 BEGIN
 
-    v_pharmacy_id := ISEQ$$_1782023.NEXTVAL;
+    INSERT INTO PHARMACY(name, address)
+    VALUES(p_name, a_name);
 
-    INSERT INTO PHARMACY(ID_PHARMACY, NAME, ADDRESS)
-    VALUES(v_pharmacy_id, p_name, a_name);
-
-    IF NOT (DOESADDRESSEXIST(a_name)) THEN
+    IF NOT (doesAddressExist(a_name)) THEN
         ADDADDRESS(a_name, a_lat, a_lon, a_alt);
     END IF;
-
+    
+    SELECT id_pharmacy INTO v_pharmacy_id
+    FROM pharmacy
+    ORDER BY id_pharmacy DESC
+    FETCH FIRST 1 ROWS ONLY;
+    
     -- By default, on Pharmacy creation, only 1 charging station will exist.
-    ADDSCOOTERPARK(v_pharmacy_id, p_limit, 1, a_name);
-    --COMMIT;
+    addPark(v_pharmacy_id, p_limit, 1, 'scooter', a_name);
+    addPark(v_pharmacy_id, p_limit, 1, 'drone', a_name);
 END;
