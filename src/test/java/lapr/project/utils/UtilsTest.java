33e6766f8446a0b0fd075bd3955de464c94088bc
António Dias <1182147@isep.ipp.pt>
2021-01-14 11:04:03 +0000

    <MEN06-191> Bug squashing

--- src/test/java/lapr/project/utils/UtilsTest.java
+++ src/test/java/lapr/project/utils/UtilsTest.java
@@ -18,9 +18,9 @@ public class UtilsTest {
 
         System.out.println("readFile");
 
-        assertEquals(new ArrayList<>(), Utils.readFile(""));
+        assertNull(Utils.readFile(""));
 
-        assertEquals(new ArrayList<>(), Utils.readFile(null));
+        assertNull(Utils.readFile(null));
 
         String fileName = "testFile.csv";
         List<String> expResult = new ArrayList<>();
@@ -39,7 +39,7 @@ public class UtilsTest {
 
         String fileName = "testFail.csv";
 
-        List<String> expResult = new ArrayList<>();
+        List<String> expResult = null;
         List<String> result = Utils.readFile(fileName);
         assertEquals(expResult, result);
     }
