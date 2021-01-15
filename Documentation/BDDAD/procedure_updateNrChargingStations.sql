b5e966844e5d0145b9252b7bb90ff2b55b32f46b
bealira <1200607@isep.ipp.pt>
2021-01-15 15:07:57 +0000

    <MEN06-247> MEN06-266 #comment correction [Implementation]

--- Documentation/BDDAD/procedure_updateNrChargingStations.sql
+++ Documentation/BDDAD/procedure_updateNrChargingStations.sql
@@ -1,6 +1,7 @@
 CREATE OR REPLACE PROCEDURE updateNrChargingStations(idPark park.id_park%type, nrCS park.num_charging_stations%type) IS
 
 num_CS INTEGER;
+vezes INTEGER;
 idps parkingspace.id_parking_space%type;
 idp parkingspace.id_park%type;
 iscs parkingspace.is_charging_station%type;
@@ -22,20 +23,28 @@ begin
     where id_park=idpark;
     
     if nrcs>num_cs then
+        vezes:=nrcs-num_cs;
         open lista_parkingspace_maior;
         loop
-            fetch lista_parkingspace_maior INTO idps,idp,iscs;
-            exit when lista_parkingspace_maior%notfound;
-            update parkingspace SET is_charging_station=1;
+            while vezes>0 loop
+                fetch lista_parkingspace_maior INTO idps,idp,iscs;
+                exit when lista_parkingspace_maior%notfound;
+                update parkingspace SET is_charging_station=1;
+                vezes:=vezes-1;
+            end loop;
         end loop;
     end if;
 
     if nrcs<num_cs then
+    vezes:=num_cs-nrcs;
         open lista_parkingspace_menor;
         loop
-            fetch lista_parkingspace_menor INTO idps,idp,iscs;
-            exit when lista_parkingspace_menor%notfound;
-            update parkingspace SET is_charging_station=0;
+            while vezes>0 loop
+                fetch lista_parkingspace_menor INTO idps,idp,iscs;
+                exit when lista_parkingspace_menor%notfound;
+                update parkingspace SET is_charging_station=0;
+                vezes:=vezes-1;
+            end loop;
         end loop;
     end if;
     
