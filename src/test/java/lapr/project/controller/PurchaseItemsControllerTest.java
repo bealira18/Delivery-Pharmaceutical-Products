a187277942e4f1d5b88028432397d5b1ea397073
Ricardo Mourisco <1170640@isep.ipp.pt>
2021-01-19 23:46:30 +0000

    <MEN06-312> Fix a bug on testing method

--- src/test/java/lapr/project/controller/PurchaseItemsControllerTest.java
+++ src/test/java/lapr/project/controller/PurchaseItemsControllerTest.java
@@ -6,7 +6,7 @@ import lapr.project.model.Pharmacy;
 import lapr.project.model.Product;
 import lapr.project.model.ProductCategory;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
