fd60224bb04343e8aa103d7bbe020feb4cdd99cf
André Silva [1171060] <1171060@isep.ipp.pt>
2021-01-20 03:25:16 +0000

    MEN06-331 finishing adding average speed to vehicle

--- src/test/java/lapr/project/model/VehicleTest.java
+++ src/test/java/lapr/project/model/VehicleTest.java
@@ -15,7 +15,7 @@ public class VehicleTest {
     public void testGetIdVehicle() {
 
         System.out.println("getIdVehicle");
-        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 8.9);
         int expResult = 1;
         int result = instance.getIdVehicle();
         assertEquals(expResult, result);
@@ -28,7 +28,7 @@ public class VehicleTest {
     public void testGetIdPharmacy() {
 
         System.out.println("getIdPharmacy");
-        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 8.9);
         int expResult = 1;
         int result = instance.getIdPharmacy();
         assertEquals(expResult, result);
@@ -41,7 +41,7 @@ public class VehicleTest {
     public void testGetWeight() {
 
         System.out.println("getWeight");
-        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 8.9);
         double expResult = 1.0;
         double result = instance.getWeight();
         assertEquals(expResult, result, 0.0);
@@ -54,7 +54,7 @@ public class VehicleTest {
     public void testGetAerodynamicCoeficient() {
 
         System.out.println("getAerodynamicCoeficient");
-        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 8.9);
         double expResult = 1.0;
         double result = instance.getAerodynamicCoeficient();
         assertEquals(expResult, result, 0.0);
@@ -67,7 +67,7 @@ public class VehicleTest {
     public void testGetFrontalArea() {
 
         System.out.println("getFrontalArea");
-        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 8.9);
         double expResult = 1.0;
         double result = instance.getFrontalArea();
         assertEquals(expResult, result, 0.0);
@@ -80,7 +80,7 @@ public class VehicleTest {
     public void testGetMotor() {
 
         System.out.println("getMotor");
-        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 8.9);
         double expResult = 1.0;
         double result = instance.getMotor();
         assertEquals(expResult, result, 0.0);
@@ -93,7 +93,7 @@ public class VehicleTest {
     public void testGetCurrentBattery() {
 
         System.out.println("getCurrentBattery");
-        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 8.9);
         double expResult = 1.0;
         double result = instance.getCurrentBattery();
         assertEquals(expResult, result, 0.0);
@@ -106,7 +106,7 @@ public class VehicleTest {
     public void testGetMaxBattery() {
 
         System.out.println("getMaxBattery");
-        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 8.9);
         double expResult = 1.0;
         double result = instance.getMaxBattery();
         assertEquals(expResult, result, 0.0);
@@ -120,7 +120,7 @@ public class VehicleTest {
 
         System.out.println("setIdVehicle");
         int idVehicle = 2;
-        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 8.9);
         instance.setIdVehicle(idVehicle);
         int result = instance.getIdVehicle();
         assertEquals(idVehicle, result);
@@ -134,7 +134,7 @@ public class VehicleTest {
 
         System.out.println("setIdPharmacy");
         int idPharmacy = 2;
-        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 8.9);
         instance.setIdPharmacy(idPharmacy);
         int result = instance.getIdPharmacy();
         assertEquals(idPharmacy, result);
@@ -148,7 +148,7 @@ public class VehicleTest {
 
         System.out.println("setWeight");
         double weight = 2.0;
-        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 8.9);
         instance.setWeight(weight);
         double result = instance.getWeight();
         assertEquals(weight, result, 0.0);
@@ -162,7 +162,7 @@ public class VehicleTest {
 
         System.out.println("setAerodynamicCoeficient");
         double aerodynamicCoeficient = 2.0;
-        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 8.9);
         instance.setAerodynamicCoeficient(aerodynamicCoeficient);
         double result = instance.getAerodynamicCoeficient();
         assertEquals(aerodynamicCoeficient, result);
@@ -176,7 +176,7 @@ public class VehicleTest {
 
         System.out.println("setFrontalArea");
         double frontalArea = 2.0;
-        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 8.9);
         instance.setFrontalArea(frontalArea);
         double result = instance.getFrontalArea();
         assertEquals(frontalArea, result);
@@ -190,7 +190,7 @@ public class VehicleTest {
 
         System.out.println("setMotor");
         double motor = 2.0;
-        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 8.9);
         instance.setMotor(motor);
         double result = instance.getMotor();
         assertEquals(motor, result);
@@ -204,7 +204,7 @@ public class VehicleTest {
 
         System.out.println("setCurrentBattery");
         double currentBattery = 2.0;
-        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 8.9);
         instance.setCurrentBattery(currentBattery);
         double result = instance.getCurrentBattery();
         assertEquals(currentBattery, result);
@@ -218,7 +218,7 @@ public class VehicleTest {
 
         System.out.println("setMaxBattery");
         double maxBattery = 2.0;
-        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 8.9);
         instance.setMaxBattery(maxBattery);
         double result = instance.getMaxBattery();
         assertEquals(maxBattery, result);
@@ -231,9 +231,9 @@ public class VehicleTest {
     public void testToString() {
 
         System.out.println("toString");
-        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 8.9);
         String expResult = "Vehicle{idVehicle=1, idPharmacy=1, weight=1.0, aerodynamicCoeficient=1.0,"
-                + " frontalArea=1.0, motor=1.0, currentBattery=1.0, maxBattery=1.0}";
+                + " frontalArea=1.0, motor=1.0, currentBattery=1.0, maxBattery=1.0, averageSpeed=8.9}";
         String result = instance.toString();
         assertEquals(expResult, result);
     }
@@ -245,7 +245,7 @@ public class VehicleTest {
     public void testHashCode() {
 
         System.out.println("hashCode");
-        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 8.9);
         int expResult = 40;
         int result = instance.hashCode();
         assertEquals(expResult, result);
@@ -259,7 +259,7 @@ public class VehicleTest {
 
         System.out.println("equals");
         Object obj = null;
-        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+        Vehicle instance = new Vehicle(1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 8.9);
         boolean expResult = true;
         boolean result = instance.equals(instance);
         assertEquals(expResult, result);
@@ -271,10 +271,10 @@ public class VehicleTest {
         result = instance.equals(obj);
         assertEquals(false, result);
 
-        result = instance.equals(new Vehicle(2, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0));
+        result = instance.equals(new Vehicle(2, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 8.9));
         assertEquals(false, result);
 
-        result = instance.equals(new Vehicle(1, 2, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0));
+        result = instance.equals(new Vehicle(1, 2, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 8.9));
         assertEquals(true, result);
     }
 }
