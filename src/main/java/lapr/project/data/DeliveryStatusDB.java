05024d7b600c5f0d1bf4101e2f15c42c047acc15
jcaboferreira <1190708@isep.ipp.pt>
2021-01-21 02:23:43 +0000

    <MEN06-279> MEN06-284 #close [Review]

--- src/main/java/lapr/project/data/DeliveryStatusDB.java
+++ src/main/java/lapr/project/data/DeliveryStatusDB.java
@@ -11,7 +11,6 @@ public class DeliveryStatusDB extends DataHandler {
         CallableStatement callStmt = null;
 
         try{
-            openConnection();
 
             callStmt = getConnection().prepareCall("{ call updateDeliveryStatusInDelivery(?) }");
 
--- src/test/java/lapr/project/controller/NotifieClientControllerTest.java
+++ src/test/java/lapr/project/controller/NotifieClientControllerTest.java
@@ -35,6 +35,10 @@ class NotifieClientControllerTest {
         when(stockDB.checkIfIsEnoughStock(order1.getId())).thenReturn(Boolean.TRUE);
         when(stockDB.checkIfIsEnoughStockInOtherPharmacy(order1.getId())).thenReturn(Boolean.TRUE);
 
+        when(deliveryStatusDB.updateDeliveryStatusInDelivery(order1.getId())).thenReturn(Boolean.TRUE);
+        when(deliveryDB.getClientEmailFromOrder(order1.getId())).thenReturn(order1.getClientEmail());
+        when(emailService.sendEmail(order1.getClientEmail(),"Delivery Run Starts", "This email is just to let you know that the delivery is on the way")).thenReturn(Boolean.TRUE);
+
         controller = new NotifyClientController();
         controller = new NotifyClientController(stockDB, deliveryDB, deliveryStatusDB, emailService);
     }
@@ -45,7 +49,7 @@ class NotifieClientControllerTest {
      * @throws java.lang.Exception
      */
     @Test
-    public void testcheckIfIsEnoughStock() throws Exception {
+    public void testCheckIfIsEnoughStock() throws Exception {
 
         System.out.println("checkIfIsEnoughtStock");
 
@@ -79,4 +83,37 @@ class NotifieClientControllerTest {
         assertEquals(true, result);
     }
 
+    /**
+     * Test of notifyClientDeliveryRunStarts method, of class NotifieClientController.
+     *
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testNotifyClientDeliveryRunStarts() throws Exception {
+        System.out.println("notifyClientDeliveryRunStarts");
+
+        PurchaseOrder order1 = new PurchaseOrder(1, 1, "teste@gmail.com", LocalDate.now());
+
+        boolean result = controller.notifyClientDeliveryRunStarts(order1);
+        assertEquals(true, result);
+
+        StockDB stockDB = mock(StockDB.class);
+        DeliveryDB deliveryDB = mock(DeliveryDB.class);
+        DeliveryStatusDB deliveryStatusDB = mock(DeliveryStatusDB.class);
+        EmailService emailService = mock(EmailService.class);
+
+        when(deliveryStatusDB.updateDeliveryStatusInDelivery(order1.getId())).thenReturn(Boolean.FALSE);
+
+        NotifyClientController controller1 = new NotifyClientController(stockDB, deliveryDB, deliveryStatusDB, emailService);
+
+        result = controller1.notifyClientDeliveryRunStarts(order1);
+        assertEquals(false, result);
+
+        when(deliveryStatusDB.updateDeliveryStatusInDelivery(order1.getId())).thenReturn(Boolean.TRUE);
+        when(deliveryDB.getClientEmailFromOrder(order1.getId())).thenReturn("teste@gmail.com");
+        when(emailService.sendEmail(order1.getClientEmail(),"Delivery Run Starts", "This email is just to let you know that the delivery is on the way")).thenReturn(Boolean.FALSE);
+
+        result = controller1.notifyClientDeliveryRunStarts(order1);
+        assertEquals(false, result);
+    }
 }
