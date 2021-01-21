b79854d8282f454dc140b1ee08b51a1a248ed21f
André Silva [1171060] <1171060@isep.ipp.pt>
2021-01-21 07:12:40 +0000

    <MEN06-30> MEN06-154 #comment test for method sendInvoiceByEmail

--- src/main/java/lapr/project/controller/CreateInvoiceController.java
+++ src/main/java/lapr/project/controller/CreateInvoiceController.java
@@ -79,16 +79,18 @@ public class CreateInvoiceController {
         emailBody.append("Order:");
         emailBody.append(System.getProperty("line.separator"));
         emailBody.append(String.format("%-40s%-10s%-10s", "Item", "Number", "Price"));
+        emailBody.append(System.getProperty("line.separator"));
         emailBody.append(slash);
         emailBody.append(System.getProperty("line.separator"));
 
         for(ProductLine productLine : productLineList) {
             Product p = productDB.getProduct(productLine.getProductId());
-            emailBody.append(String.format("%-40s%-10d€%-10f", p.getName(), productLine.getProductQuantity(), productLine.getPrice()));
+            emailBody.append(String.format("%-40s%-10d€%-10.2f", p.getName(), productLine.getProductQuantity(), productLine.getPrice()));
+            emailBody.append(System.getProperty("line.separator"));
             emailBody.append(slash);
             emailBody.append(System.getProperty("line.separator"));
         }
-        emailBody.append(String.format("%50s%f", "€", totalPrice));
+        emailBody.append(String.format("%51s%.2f", "€", totalPrice));
         emailBody.append(System.getProperty("line.separator"));
         emailBody.append(System.getProperty("line.separator"));
         emailBody.append("NIF: ").append(client.getNif());
