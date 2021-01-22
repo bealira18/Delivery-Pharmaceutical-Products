8729db30222d5e969dbb2198723e3d378b9de1cd
Ricardo Mourisco <1170640@isep.ipp.pt>
2021-01-22 19:55:04 +0000

    <MEN06-21> MEN06-144 #comment Java sided listener, writer and interpreter for parking interaction [Implementation]

---
+++ src/main/java/lapr/project/controller/VehicleParkingController.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.controller;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.List;
+import java.util.Scanner;
+import lapr.project.data.CourierDB;
+import lapr.project.data.EmailService;
+import lapr.project.data.VehicleDB;
+import lapr.project.model.Vehicle;
+import lapr.project.utils.Utils;
+
+
+/**
+ *
+ * @author Ricardo
+ */
+public class VehicleParkingController {
+    
+    public final VehicleDB vDB;
+    public final EmailService eS;
+
+    public VehicleParkingController(VehicleDB vDB, EmailService eS) {
+        this.vDB = vDB;
+        this.eS = eS;
+    }
+
+    public VehicleParkingController() {
+        this.vDB = new VehicleDB();
+        this.eS = new EmailService();
+    }
+
+    public void interpretChargerInfo(String fileName) {
+        
+        try {
+            File fileBuffer = new File(System.getProperty("charger.comm.dir") + fileName);
+            Scanner sc = new Scanner(fileBuffer);
+            String buffer = sc.nextLine();
+            
+            String[] arrBuffer = buffer.split(";");
+            
+            if (arrBuffer.length != 3)
+            {
+                throw new IllegalArgumentException("Unexpected number of arguments");
+            }
+            
+            //Check if the vehicle ID is real and what type it is
+            String vehicleType = vDB.typeOfVehicleByID(Integer.parseInt(arrBuffer[1]));
+            List<String> nameNemail = vDB.getEmailNameFromParkedVehicleResponsible(Integer.parseInt(arrBuffer[1]));
+            LocalDateTime date;
+            
+            date = LocalDateTime.ofInstant(Instant.ofEpochSecond(Long.parseLong(arrBuffer[0])), ZoneId.of("Z"));
+            
+            sendStatusEmail(nameNemail.get(1), nameNemail.get(0), date, Integer.parseInt(arrBuffer[1]), vehicleType, Float.parseFloat(arrBuffer[2]));
+                        
+        } catch (FileNotFoundException | SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public void writeChargerRequest(Vehicle vehicle, boolean isReal)
+    {
+        LocalDateTime date = LocalDateTime.now();
+        long timestamp = date.toEpochSecond(ZoneOffset.UTC);
+        
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("'lock_'yyyy'_'MM'_'dd'_'HH'_'mm'_'ss'.data'");
+        String fileName = System.getProperty("charger.comm.dir") + date.format(dtf);
+
+        String buffer;
+        
+        buffer = String.format("%d;%d", timestamp, vehicle.getIdVehicle());
+        
+        if (isReal)
+        {
+            //Yes, I'm aware I'm writing longs to a system that expects ints: timestamp wont be a problem until 2038, the others will never crack the integer limit.
+            buffer = String.format("%s;%d;%d", buffer, Math.round(vehicle.getMaxBattery()), Math.round(vehicle.getCurrentBattery()));
+        }
+        
+        Utils.writeFile(buffer, fileName);
+        Utils.writeFile(" ", fileName+".flag");
+    }
+
+    private void sendStatusEmail(String email, String name, LocalDateTime date, int vehicleID, String vehicleType, float timeToFull) {
+        StringBuilder buildStatus = new StringBuilder("Caro Sr/a ");
+        buildStatus.append(name).append(",");
+        buildStatus.append(System.getProperty("line.separator"));
+        buildStatus.append(System.getProperty("line.separator"));
+        buildStatus.append("O veículo do tipo ").append(vehicleType).append(" de ID ").append(vehicleID).append(" estacionado às ");
+        buildStatus.append(date.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)));
+        
+        if(timeToFull < 0.0)
+        {
+            buildStatus.append(" foi mal estacionado.").append(System.getProperty("line.separator"));
+            buildStatus.append("Por favor, regresse ao parque e estacione o veículo apropriadamente.");
+        }
+        else
+        {
+            buildStatus.append(" foi propriamente estacionado.").append(System.getProperty("line.separator"));
+            buildStatus.append(String.format("A bateria desta estará carregada em %.2f horas.", timeToFull));
+        }
+        
+        String body = String.format("Caro Sr/a %s, \n \nO veículo do tipo %s de ID %d estacionado às %s foi mal estacionado. \nPor favor, regresse ao parque e estacione o veículo apropriadamente.", name, vehicleType, vehicleID, date.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)));
+        eS.sendEmail(email, "Notificação de estacionamento", body);
+    }    
+}
---
+++ src/main/java/lapr/project/data/AssemblyWatcher.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.data;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import lapr.project.controller.VehicleParkingController;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class AssemblyWatcher implements Runnable {
+
+    public WatchService asmWatchService = null;
+
+    public AssemblyWatcher() {
+        try {
+            asmWatchService = FileSystems.getDefault().newWatchService();
+
+            Path path = Paths.get(System.getProperty("charger.comm.dir"));
+
+            path.register(asmWatchService, StandardWatchEventKinds.ENTRY_CREATE);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+
+        Pattern pattern = Pattern.compile("^estimate_[0-9]{4}_[0-9]{2}_[0-9]{2}_[0-9]{2}_[0-9]{2}_[0-9]{2}[.]data[.]flag$", Pattern.CASE_INSENSITIVE);
+        Matcher matcher;
+        WatchKey key;
+        try {
+            while ((key = asmWatchService.take()) != null) {
+                for (WatchEvent<?> event : key.pollEvents()) {
+                    
+                    matcher = pattern.matcher(event.context().toString());
+                    System.out.println(event.context().toString());
+                    
+                    if (matcher.find()) {
+                        new VehicleParkingController().interpretChargerInfo(event.context().toString().replace(".flag", ""));
+                        File flag = new File(System.getProperty("charger.comm.dir") + event.context().toString());
+                        if (flag.exists())
+                            flag.delete();
+                    }
+
+                }
+                key.reset();
+            }
+        } catch (InterruptedException ex) {
+            Logger.getLogger(AssemblyWatcher.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
