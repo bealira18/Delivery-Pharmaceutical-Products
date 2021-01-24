87447c882f56827d9b680019ada38df567e3b1fb
André Silva [1171060] <1171060@isep.ipp.pt>
2021-01-24 03:51:50 +0000

    MEN06-338 #comment tests for UseCreditsController

--- src/test/java/lapr/project/controller/UseCreditsControllerTest.java
+++ src/test/java/lapr/project/controller/UseCreditsControllerTest.java
@@ -7,8 +7,10 @@ import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class UseCreditsControllerTest {
 
@@ -18,14 +20,39 @@ public class UseCreditsControllerTest {
     }
 
     @BeforeAll
-    public static void setUpClass() {
+    public static void setUpClass() throws SQLException {
 
         ClientDB cDB = mock(ClientDB.class);
+
+        when(cDB.getCreditsByClientEmail("test@gmail.com")).thenReturn(5);
+        when(cDB.updateCreditsClient("test@gmail.com", 0)).thenReturn(Boolean.TRUE);
 
         controller = new UseCreditsController();
         controller = new UseCreditsController(cDB);
     }
 
+    @Test
+    public void testGetCreditsByClientEmail() throws SQLException {
 
+        int expResult = 5;
+        int result = controller.getCreditsByClientEmail("test@gmail.com");
+        assertEquals(expResult, result);
+    }
 
+    @Test
+    public void testUpdateCreditsClient() throws SQLException {
+
+        boolean expResult = true;
+        boolean result = controller.updateCreditsClient("test@gmail.com", 0);
+        assertEquals(expResult, result);
+
+        ClientDB cDB = mock(ClientDB.class);
+
+        when(cDB.updateCreditsClient("test@gmail.com", 0)).thenReturn(Boolean.FALSE);
+        UseCreditsController controller2 = new UseCreditsController(cDB);
+
+        expResult = false;
+        result = controller2.updateCreditsClient("test@gmail.com", 0);
+        assertEquals(expResult, result);
+    }
 }
