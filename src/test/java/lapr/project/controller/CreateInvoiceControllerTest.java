b424adfcfa69ce7837aea5ce5f16d579c5267577
André Silva [1171060] <1171060@isep.ipp.pt>
2021-01-25 18:58:37 +0000

    MEN06-338 #comment test fix

--- src/test/java/lapr/project/controller/CreateInvoiceControllerTest.java
+++ src/test/java/lapr/project/controller/CreateInvoiceControllerTest.java
@@ -74,7 +74,7 @@ class CreateInvoiceControllerTest {
                 System.getProperty("line.separator") +
                 "Total: 12.90€" +
                 System.getProperty("line.separator") +
-                "Total w/o IVA: 10.32     IVA: 23%" +
+                "Total w/o IVA: €10.32     IVA: 23%" +
                 System.getProperty("line.separator") +
                 "NIF: 1")).thenReturn(Boolean.TRUE);
         when(manageCreditsController.payDeliveryFee(purchaseOrder.getClientEmail())).thenReturn(Boolean.TRUE);
@@ -180,7 +180,7 @@ class CreateInvoiceControllerTest {
                 System.getProperty("line.separator") +
                 "Total: 12.90€" +
                 System.getProperty("line.separator") +
-                "Total w/o IVA: 10.32     IVA: 23%" +
+                "Total w/o IVA: €10.32     IVA: 23%" +
                 System.getProperty("line.separator") +
                 "NIF: 1")).thenReturn(Boolean.FALSE);
 
@@ -222,7 +222,7 @@ class CreateInvoiceControllerTest {
                 System.getProperty("line.separator") +
                 "Total: 12.90€" +
                 System.getProperty("line.separator") +
-                "Total w/o IVA: 10.32     IVA: 23%" +
+                "Total w/o IVA: €10.32     IVA: 23%" +
                 System.getProperty("line.separator") +
                 "NIF: 1";
 
