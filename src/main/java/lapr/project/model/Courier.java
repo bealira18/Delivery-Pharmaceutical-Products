3216e121fa1df452554ecb8580e9e1501f248707
Ricardo Mourisco <1170640@isep.ipp.pt>
2021-01-30 01:23:33 +0000

    MEN06-338 #comment Javadoc for Courier and CreditCard [Implementation]

--- src/main/java/lapr/project/model/Courier.java
+++ src/main/java/lapr/project/model/Courier.java
@@ -1,13 +1,48 @@
 package lapr.project.model;
 
+/**
+ * This class serves as a container for the Courier role and it's specific
+ * information.
+ * @author lapr3-2020-g052
+ */
 public class Courier extends RegisteredUser {
 
+    /**
+     * The name of the courier.
+     */
     private String name;
+
+    /**
+     * The NIF of the courier.
+     */
     private int nif;
+
+    /**
+     * The Social Security number of the courier.
+     */
     private long socialSecurity;
+
+    /**
+     * The id of the pharmacy that this courier is associated to.
+     */
     private int pharmacyId;
+
+    /**
+     * The weight of the couier.
+     */
     private double weight;
 
+    /**
+     * Creates a instance of the Courier Object with the given arguments.
+     *
+     * @param email the email of the courier.
+     * @param password the password of the courier.
+     * @param name the name of the courier.
+     * @param nif the nif of the courier.
+     * @param socialSecurity the social security number of the courier.
+     * @param pharmacyId the pharmacy ID associated to the courier.
+     * @param weight the weight of the courier.
+     */
     public Courier(String email, String password, String name, int nif, long socialSecurity, int pharmacyId, double weight) {
 
         super(email, password, "Courier");
@@ -18,61 +53,109 @@ public class Courier extends RegisteredUser {
         this.weight = weight;
     }
 
+    /**
+     * Retrieves the name field of this courier object.
+     * @return string containing the name.
+     */
     public String getName() {
 
         return name;
     }
 
+    /**
+     * Retrieves the NIF field of this courier object.
+     * @return int containing the NIF.
+     */
     public int getNif() {
 
         return nif;
     }
 
+    /**
+     * Retrieves the Social Security field of this courier object.
+     * @return long containing the Social Security number.
+     */
     public long getSocialSecurity() {
 
         return socialSecurity;
     }
 
+    /**
+     * Retrieves the pharmacy ID field of this courier object.
+     * @return int containing the pharmacy ID.
+     */
     public int getPharmacyId() {
 
         return pharmacyId;
     }
 
+    /**
+     * Retrieves the weight value of this courier object.
+     * @return double containing the weight.
+     */
     public double getWeight() {
 
         return weight;
     }
 
+    /**
+     * Sets a new name for this courier object.
+     * @param name new name.
+     */
     public void setName(String name) {
 
         this.name = name;
     }
 
+    /**
+     * Sets a new NIF for this courier object.
+     * @param nif new NIF.
+     */
     public void setNif(int nif) {
 
         this.nif = nif;
     }
 
+    /**
+     * Sets a new Social Security Number for this courier object.
+     * @param socialSecurity new social security number.
+     */
     public void setSocialSecurity(long socialSecurity) {
 
         this.socialSecurity = socialSecurity;
     }
 
+    /**
+     * Sets a new pharmacy ID for this courier object.
+     * @param pharmacyId new pharmacy ID.
+     */
     public void setPharmacyId(int pharmacyId) {
 
         this.pharmacyId = pharmacyId;
     }
 
+    /**
+     * Sets a new weight for this courier object.
+     * @param weight new weight.
+     */
     public void setWeight(double weight) {
 
         this.weight = weight;
     }
-
+    
+    /**
+     * Courier String format.
+     * @return string with this courier information.
+     */
     @Override
     public String toString() {
         return "Courier{" + "name=" + name + ", nif=" + nif + ", socialSecurity=" + socialSecurity + ", pharmacyId=" + pharmacyId + ", weight=" + weight + '}';
     }   
-
+    
+    /**
+     * Returns the hash code for this Courier object.
+     * @return hash code for the object.
+     */
     @Override
     public int hashCode() {
 
@@ -80,7 +163,13 @@ public class Courier extends RegisteredUser {
         hash = 37 * hash + this.nif;
         return hash;
     }
-
+    
+    /**
+     * Compares a Courier object to another object and checks if they are
+     * equal.
+     * @param obj the object to compare to.
+     * @return true if equal, false if not.
+     */
     @Override
     public boolean equals(Object obj) {
 
--- src/main/java/lapr/project/model/CreditCard.java
+++ src/main/java/lapr/project/model/CreditCard.java
@@ -2,12 +2,34 @@ package lapr.project.model;
 
 import java.time.LocalDate;
 
+/**
+ * This class serves as a container for the Credit Card information.
+ * 
+ * @author lapr3-2020-g052
+ */
 public class CreditCard {
 
+    /**
+     * The credit card number.
+     */
     private long creditCardNumber;
+
+    /**
+     * The credit cards expiration date.
+     */
     private LocalDate validityDate;
+
+    /**
+     * The credit cards CCV.
+     */
     private short ccv;
 
+    /**
+     * Creates a instance of the CreditCard Object with the given arguments.
+     * @param creditCardNumber
+     * @param validityDate
+     * @param ccv
+     */
     public CreditCard(long creditCardNumber, LocalDate validityDate, short ccv) {
 
         this.creditCardNumber = creditCardNumber;
@@ -15,42 +37,74 @@ public class CreditCard {
         this.ccv = ccv;
     }
 
+    /**
+     * Retrieves the Credit Card Number field of this CreditCard object.
+     * @return long containing the credit card number.
+     */
     public long getCreditCardNumber() {
 
         return creditCardNumber;
     }
 
+    /**
+     * Retrieves the Expiration Date field of this CreditCard object.
+     * @return LocalDate containing the Expiration Date.
+     */
     public LocalDate getValidityDate() {
 
         return validityDate;
     }
 
+    /**
+     * Retrieves the CCV field of this CreditCard object.
+     * @return short containing the CCV.
+     */
     public short getCcv() {
 
         return ccv;
     }
 
+    /**
+     * Sets a new Credit Card number for this CreditCard object.
+     * @param creditCardNumber new Credit Card number.
+     */
     public void setCreditCardNumber(long creditCardNumber) {
 
         this.creditCardNumber = creditCardNumber;
     }
 
+    /**
+     * Sets a new Credit Card expiration date for this CreditCard object.
+     * @param validityDate new Credit card expiration date.
+     */
     public final void setValidityDate(LocalDate validityDate) {
 
         this.validityDate = validityDate;
     }
 
+    /**
+     * Sets a new CCV for this CreditCard object.
+     * @param ccv new CCV.
+     */
     public void setCcv(short ccv) {
 
         this.ccv = ccv;
     }
-
+    
+    /**
+     * CreditCard String format.
+     * @return string with this CreditCard information.
+     */
     @Override
     public String toString() {
 
         return "CreditCard{" + "creditCardNumber=" + creditCardNumber + ", validityDate=" + validityDate + ", ccv=" + ccv + '}';
     }
 
+    /**
+     * Returns the hash code for this CreditCard object.
+     * @return hash code for the object.
+     */
     @Override
     public int hashCode() {
 
@@ -58,7 +112,12 @@ public class CreditCard {
         hash = 97 * hash + (int)(this.creditCardNumber ^ (this.creditCardNumber >>> 32));
         return hash;
     }
-
+    
+    /**
+     * Compares a CreditCard object to another object and checks if they are equal.
+     * @param obj the object to compare to.
+     * @return true if equal, false if not.
+     */
     @Override
     public boolean equals(Object obj) {
 
