cd1a960cc8f2fc427f479ce289f4de4ef3215452
Ricardo Mourisco <1170640@isep.ipp.pt>
2021-01-30 22:21:01 +0000

    MEN06-338 #comment Javadoc for EmailService [Implementation]

--- src/main/java/lapr/project/data/EmailService.java
+++ src/main/java/lapr/project/data/EmailService.java
@@ -10,10 +10,23 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+/**
+ * This class serves as the interface that the application can use to send email
+ * notifications, invoices or any other type of electronic correspondence.
+ * 
+ * @author lapr3-2020-g052
+ */
 public class EmailService {
 
+    /**
+     * The applications email session object, used to send the emails.
+     */
     private Session emailSession;
 
+    /**
+     * Initializes the Email interface, by creating a new session and properly 
+     * authenticate with the information from the configuration file.
+     */
     public EmailService() {
 
         emailSession = Session.getInstance(System.getProperties(), new javax.mail.Authenticator() {
@@ -23,12 +36,20 @@ public class EmailService {
         });
     }
 
-    public boolean sendEmail(String destinationEmaail, String subjectLine, String emailBody) {
+    /**
+     * Sends a email to a valid destination with the provided subject line and email body.
+     *
+     * @param destinationEmail a valid email destination.
+     * @param subjectLine the subject of the email.
+     * @param emailBody the email text body.
+     * @return true on success, false on a MessageException (ie, invalid email).
+     */
+    public boolean sendEmail(String destinationEmail, String subjectLine, String emailBody) {
 
         try {
             Message message = new MimeMessage(emailSession);
             message.setFrom(new InternetAddress(System.getProperty("mail.username")));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(destinationEmaail));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(destinationEmail));
             message.setSubject(subjectLine);
             message.setText(emailBody);
 
