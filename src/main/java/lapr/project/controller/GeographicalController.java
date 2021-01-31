b908e3f2be4cf40da61d7bd0b6151a3d8987e0af
António Dias <1182147@isep.ipp.pt>
2021-01-31 17:39:47 +0000

    <MEN06-338> #comment Javadoc for Graph and Geographical Controllers.

--- src/main/java/lapr/project/controller/GeographicalController.java
+++ src/main/java/lapr/project/controller/GeographicalController.java
@@ -6,23 +6,51 @@ import lapr.project.data.PathDB;
 import lapr.project.model.Address;
 import lapr.project.model.Path;
 
+/**
+ * Controls the processes pertinent to geographical point and path manipulation
+ * and creation.
+ *
+ * @author lapr3-2020-g052
+ */
 public class GeographicalController {
 
+    /**
+     * The AddressDB instance, in order to access the pertinent Database.
+     */
     private final AddressDB aDB;
+    /**
+     * The PathDB instance, in order to access the pertinent Database.
+     */
     private final PathDB pDB;
 
+    /**
+     * Creates an instance of a GeographicalController.
+     */
     public GeographicalController() {
 
         aDB = new AddressDB();
         pDB = new PathDB();
     }
 
+    /**
+     * Creates an instance of a GeographicalController with the Database
+     * instances relevant to this object received as parameters.
+     *
+     * @param aDB
+     * @param pDB
+     */
     public GeographicalController(AddressDB aDB, PathDB pDB) {
 
         this.aDB = aDB;
         this.pDB = pDB;
     }
 
+    /**
+     * Initializes the process of adding a set of addresses, received as
+     * parameters, to the Database.
+     *
+     * @param la the list of addresses to be added.
+     */
     public void addAddresses(List<Address> la) {
 
         for (Address a : la) {
@@ -30,25 +58,57 @@ public class GeographicalController {
         }
     }
 
+    /**
+     * Initializes the process of adding an address, received as parameter, to
+     * the Database.
+     *
+     * @param a the address to be added.
+     * @return true, if it has been added successfully. Otherwise, returns
+     * false.
+     */
     public boolean addAddress(Address a) {
 
         return aDB.addAddress(a);
     }
 
+    /**
+     * Returns all the addresses contained in the Database.
+     *
+     * @return all the addresses contained in the Database.
+     */
     public List<Address> getAddresses() {
 
         return aDB.getAddresses();
     }
 
+    /**
+     * Returns all the addresses where pharmacies are stationed.
+     *
+     * @return all the addresses where pharmacies are stationed.
+     */
     public List<Address> getPharmacyAddresses() {
 
         return aDB.getPharmacyAddresses();
     }
 
+    /**
+     * Returns the list of paths whose end points are contained in the list of
+     * addresses received as parameter.
+     *
+     * @param la the list of addresses.
+     * @return the list of paths whose end points are contained in the list of
+     * addresses received as parameter.
+     */
     public List<Path> getPaths(List<Address> la) {
         return pDB.getPaths(la);
     }
 
+    /**
+     * Initializes the process of adding a set of paths, received as parameters,
+     * to the Database.
+     *
+     * @param lp the list of paths to be added.
+     */
     public void addPaths(List<Path> lp) {
 
         for (Path p : lp) {
@@ -56,19 +116,43 @@ public class GeographicalController {
         }
     }
 
+    /**
+     * Initializes the process of adding a path, received as parameter, to the
+     * Database.
+     *
+     * @param p the path to be added.
+     * @return true, if it has been added successfully. Otherwise, returns
+     * false.
+     */
     public boolean addPath(Path p) {
 
         return pDB.addPath(p);
     }
 
+    /**
+     * Initializes the process of updating a path with the contents of the
+     * parameter received.
+     *
+     * @param p the path containing the changes to be made.
+     * @return true, if it has been updated successfully. Otherwise, returns
+     * false.
+     */
     public boolean updatePath(Path p) {
 
         return pDB.updatePath(p);
     }
 
+    /**
+     * Removes a path, whose endpoints are the addresses received as parameters,
+     * from the Database.
+     *
+     * @param a1 the address from the left endpoint.
+     * @param a2 the address from the right endpoint.
+     * @return true, if it has been removed successfully. Otherwise, returns
+     * false.
+     */
     public boolean removePath(Address a1, Address a2) {
 
         return pDB.removePath(a1, a2);
     }
-
 }
--- src/main/java/lapr/project/controller/GraphController.java
+++ src/main/java/lapr/project/controller/GraphController.java
@@ -11,13 +11,33 @@ import lapr.project.model.Scooter;
 import lapr.project.utils.Graph;
 import lapr.project.utils.GraphAlgorithms;
 
+/**
+ * Controls the processes pertinent to graph manipulation and creation.
+ *
+ * @author lapr3-2020-g052
+ */
 public class GraphController {
 
+    /**
+     * The scooter graph whose weights are defined by distance.
+     */
     private final Graph<Address, Path> gScooter;
+    /**
+     * The drone graph whose weights are defined by distance.
+     */
     private final Graph<Address, Path> gDrone;
+    /**
+     * The scooter graph whose weights are defined by energy.
+     */
     private final Graph<Address, Path> gScooterEnergy;
+    /**
+     * The drone graph whose weights are defined by energy.
+     */
     private final Graph<Address, Path> gDroneEnergy;
 
+    /**
+     * Creates an instance of a GraphController.
+     */
     public GraphController() {
 
         gScooter = new Graph<>(true);
@@ -26,51 +46,121 @@ public class GraphController {
         gDroneEnergy = new Graph<>(true);
     }
 
+    /**
+     * Returns the scooter graph whose weights are defined by distance.
+     *
+     * @return the scooter graph whose weights are defined by distance.
+     */
     public Graph<Address, Path> getGraphScooter() {
 
         return gScooter.clone();
     }
 
+    /**
+     * Returns the drone graph whose weights are defined by distance.
+     *
+     * @return the drone graph whose weights are defined by distance.
+     */
     public Graph<Address, Path> getGraphDrone() {
 
         return gDrone.clone();
     }
 
+    /**
+     * Returns the scooter graph whose weights are defined by energy.
+     *
+     * @return the scooter graph whose weights are defined by energy.
+     */
     public Graph<Address, Path> getGraphScooterEnergy() {
 
         return gScooterEnergy.clone();
     }
 
+    /**
+     * Returns the drone graph whose weights are defined by energy.
+     *
+     * @return the drone graph whose weights are defined by energy.
+     */
     public Graph<Address, Path> getGraphDroneEnergy() {
 
         return gDroneEnergy.clone();
     }
 
+    /**
+     * Initializes the process of filling the scooter graph whose weights are
+     * defined by distance.
+     *
+     * @param la the list of addresses (vertexes).
+     * @param lp the list of paths (edges).
+     */
     public void fillGraphScooter(List<Address> la, List<Path> lp) {
 
         GraphAlgorithms.fillGraph(gScooter, la, lp);
     }
 
+    /**
+     * Initializes the process of filling the drone graph whose weights are
+     * defined by distance.
+     *
+     * @param la the list of addresses (vertexes).
+     * @param lp the list of paths (edges).
+     */
     public void fillGraphDrone(List<Address> la, List<Path> lp) {
 
         GraphAlgorithms.fillGraph(gDrone, la, lp);
     }
 
+    /**
+     * Initializes the process of filling the scooter graph whose weights are
+     * defined by energy.
+     *
+     * @param la the list of addresses (vertexes).
+     * @param lp the list of paths (edges).
+     */
     public void fillGraphScooterEnergy(List<Address> la, List<Path> lp) {
 
         GraphAlgorithms.fillGraphEnergy(true, gScooterEnergy, la, lp);
     }
 
+    /**
+     * Initializes the process of filling the drone graph whose weights are
+     * defined by energy.
+     *
+     * @param la the list of addresses (vertexes).
+     * @param lp the list of paths (edges).
+     */
     public void fillGraphDroneEnergy(List<Address> la, List<Path> lp) {
 
         GraphAlgorithms.fillGraphEnergy(false, gDroneEnergy, la, lp);
     }
 
+    /**
+     * Initializes the process of obtaining the nearest pharmacy to a given
+     * address, received as parameter.
+     *
+     * @param inverted true, if it's to calculate from the pharmacy to the
+     * client, false if otherwise.
+     * @param a the target address.
+     * @param laPharma the list of pharmacy addresses.
+     * @return the nearest pharmacy to the given address.
+     */
     public Address getNearestPharmacy(boolean inverted, Address a, List<Address> laPharma) {
 
         return GraphAlgorithms.getNearestPharmacy(inverted, gDrone, a, laPharma);
     }
 
+    /**
+     * Initializes the process of calculating and obtaining the shortest path,
+     * distance wise, between two addresses, received as parameters.
+     *
+     * @param scooterOrDrone true, if the vehicle in question to traverse in is
+     * a scooter, false if it's a drone.
+     * @param aOrig the origin address.
+     * @param aDest the destination address.
+     * @param shortPath the linked list which'll house the shortest path.
+     * @return the accumulated weight (in this case, distance (meters)) of the
+     * shortest path.
+     */
     public double getShortestPath(boolean scooterOrDrone, Address aOrig, Address aDest, LinkedList<Address> shortPath) {
 
         if (scooterOrDrone) {
@@ -81,6 +171,18 @@ public class GraphController {
         }
     }
 
+    /**
+     * Initializes the process of calculating and obtaining the shortest path,
+     * energy wise, between two addresses, received as parameters.
+     *
+     * @param scooterOrDrone true, if the vehicle in question to traverse in is
+     * a scooter, false if it's a drone.
+     * @param aOrig the origin address.
+     * @param aDest the destination address.
+     * @param shortPath the linked list which'll house the shortest path.
+     * @return the accumulated weight (in this case, energy (Wattshour)) of the
+     * shortest path.
+     */
     public double getShortestPathEnergy(boolean scooterOrDrone, Address aOrig, Address aDest, LinkedList<Address> shortPath) {
 
         if (scooterOrDrone) {
@@ -91,6 +193,21 @@ public class GraphController {
         }
     }
 
+    /**
+     * Initializes the process of calculating and obtaining the shortest path,
+     * distance wise, between two addresses, received as parameters, while
+     * obligatorily passing through a predetermined set of nodes, also received
+     * as parameter.
+     *
+     * @param scooterOrDrone true, if the vehicle in question to traverse in is
+     * a scooter, false if it's a drone.
+     * @param aOrig the origin address.
+     * @param aDest the destination address.
+     * @param nodes the nodes to be passed through compulsorily
+     * @param shortPath the linked list which'll house the shortest path.
+     * @return the accumulated weight (in this case, distance (meters)) of the
+     * shortest path.
+     */
     public double getShortestPathThroughNodes(boolean scooterOrDrone, Address aOrig, Address aDest,
             List<Address> nodes, LinkedList<Address> shortPath) {
 
@@ -102,6 +219,21 @@ public class GraphController {
         }
     }
 
+    /**
+     * Initializes the process of calculating and obtaining the shortest path,
+     * energy wise, between two addresses, received as parameters, while
+     * obligatorily passing through a predetermined set of nodes, also received
+     * as parameter.
+     *
+     * @param scooterOrDrone true, if the vehicle in question to traverse in is
+     * a scooter, false if it's a drone.
+     * @param aOrig the origin address.
+     * @param aDest the destination address.
+     * @param nodes the nodes to be passed through compulsorily
+     * @param shortPath the linked list which'll house the shortest path.
+     * @return the accumulated weight (in this case, energy (Wattshour)) of the
+     * shortest path.
+     */
     public double getShortestPathThroughNodesEnergy(boolean scooterOrDrone, Address aOrig, Address aDest,
             List<Address> nodes, LinkedList<Address> shortPath) {
 
@@ -113,6 +245,23 @@ public class GraphController {
         }
     }
 
+    /**
+     * Initializes the process of exporting a voyage, received as parameter, to
+     * a file whose name is also received as parameter. This particular
+     * iteration of the writePathToFile algorithm, which is overloaded, handles
+     * exporting voyages where the vehicle and other extraneous factors have
+     * default values.
+     *
+     * @param scooterOrDrone true, if the vehicle in question to traverse in is
+     * a scooter, false if it's a drone.
+     * @param fileName the file's name.
+     * @param la the voyage.
+     * @param distance the total distance (in meters).
+     * @param energy the total energy (in Wattshour).
+     * @param v the vehicle type.
+     * @return true, if it has been exported successfully. Otherwise, returns
+     * false.
+     */
     public boolean writePathToFile(boolean scooterOrDrone, String fileName, LinkedList<Address> la,
             double distance, double energy, String v) {
 
@@ -124,12 +273,46 @@ public class GraphController {
         }
     }
 
+    /**
+     * Initializes the process of exporting a voyage, received as parameter, to
+     * a file whose name is also received as parameter. This particular
+     * iteration of the writePathToFile algorithm, which is overloaded, handles
+     * exporting voyages where the vehicle type is a scooter and the extraneous
+     * factors such as the list of products it carried and who drived the
+     * scooter are known.
+     *
+     * @param fileName the file's name.
+     * @param la the voyage.
+     * @param distance the total distance (in meters).
+     * @param energy the total energy (in Wattshour).
+     * @param c the courier.
+     * @param s the scooter.
+     * @param lpro the list of products.
+     * @return true, if it has been exported successfully. Otherwise, returns
+     * false.
+     */
     public boolean writePathToFile(String fileName, LinkedList<Address> la, double distance, double energy,
             Courier c, Scooter s, List<Product> lpro) {
 
         return GraphAlgorithms.writePathToFile(fileName, gScooterEnergy, la, distance, energy, c, s, lpro);
     }
 
+    /**
+     * Initializes the process of exporting a voyage, received as parameter, to
+     * a file whose name is also received as parameter. This particular
+     * iteration of the writePathToFile algorithm, which is overloaded, handles
+     * exporting voyages where the vehicle type is a drone and the extraneous
+     * factors such as the list of products it carried are known.
+     *
+     * @param fileName the file's name.
+     * @param la the voyage.
+     * @param distance the total distance (in meters).
+     * @param energy the total energy (in Wattshour).
+     * @param d the drone.
+     * @param lpro the list of products.
+     * @return true, if it has been exported successfully. Otherwise, returns
+     * false.
+     */
     public boolean writePathToFile(String fileName, LinkedList<Address> la, double distance, double energy,
             Drone d, List<Product> lpro) {
 
