4538e2c195c53d1e24ceeb5966ca3f54620ed4f6
André Silva [1171060] <1171060@isep.ipp.pt>
2021-01-31 07:33:01 +0000

    MEN06-338 #comment javadocs for GetDronesController and GetProductsController

--- src/main/java/lapr/project/controller/GetDronesController.java
+++ src/main/java/lapr/project/controller/GetDronesController.java
@@ -5,20 +5,40 @@ import lapr.project.model.Drone;
 
 import java.util.List;
 
+/**
+ * Controls the process of getting the drones available to make a delivery.
+ *
+ * @author lapr3-2020-g052
+ */
 public class GetDronesController {
 
+    /**
+     * DroneDB instance to get the available drones
+     */
     private final DroneDB dDB;
 
+    /**
+     * Creates a instance of GetDronesController, creating the required instances.
+     */
     public GetDronesController() {
 
         dDB = new DroneDB();
     }
 
+    /**
+     * Creates a instance of GetDronesController with the given instances.
+     * @param dDB DroneDB instance
+     */
     public GetDronesController(DroneDB dDB) {
 
         this.dDB = dDB;
     }
 
+    /**
+     * Gets a list with the available drones to make a delivery
+     * @param orderId order to be delivered
+     * @return list with available drones
+     */
     public List<Drone> getDrones(int orderId) {
 
         return dDB.getAllAvailableDrones(orderId);
--- src/main/java/lapr/project/controller/GetProductsController.java
+++ src/main/java/lapr/project/controller/GetProductsController.java
@@ -4,20 +4,39 @@ import java.util.List;
 import lapr.project.data.ProductDB;
 import lapr.project.model.Product;
 
+/**
+ * Controls the process of getting all products in the system.
+ *
+ * @author lapr3-2020-g052
+ */
 public class GetProductsController {
 
+    /**
+     * ProductDB instance to get the registered products
+     */
     private final ProductDB pDB;
 
+    /**
+     * Creates a instance of GetProductsController, creating the required instances.
+     */
     public GetProductsController() {
 
         pDB = new ProductDB();
     }
 
+    /**
+     * Creates a instance of GetProductsController with the given instances.
+     * @param pDB ProductDB instance
+     */
     public GetProductsController(ProductDB pDB) {
 
         this.pDB = pDB;
     }
 
+    /**
+     * Gets all products in the database
+     * @return list with all products
+     */
     public List<Product> getProducts() {
 
         return pDB.getProducts();
